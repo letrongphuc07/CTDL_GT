d9e35eced57b8e0cd04c17585594ec41e2439c07
letrongphuc07 <125432003+letrongphuc07@users.noreply.github.com>
2023-04-06 07:33:29 +0700

    Create CTDL&GT_Insertion_Sort_1.cpp

---
+++ CTDL&GT_Insertion_Sort_1.cpp
@@ -0,0 +1,13 @@
+nguồn tham khảo: https://viblo.asia/p/sap-xep-chen-sap-xep-chon-va-sap-xep-tron-Do754zX4ZM6
+void Insertion_sort(int a[], int n) {
+	int index, new_number;
+	for (int i = 1; i < n; i++){
+		index = i;
+		new_number = a[i];
+		while (index > 0 && a[index - 1] > new_number){
+			a[index] = a[index - 1];
+			index--;
+		}
+		a[index] = new_number;
+	}
+}
