942e97a151f03be57aa79d4d880565ef3d8712ae
letrongphuc07 <125432003+letrongphuc07@users.noreply.github.com>
2023-04-06 10:40:03 +0700

    Create CTDL&GT_Queue_1.cpp

---
+++ CTDL&GT_Queue_1.cpp
@@ -0,0 +1,34 @@
+nguồn tham khảo: https://devmaster.edu.vn/cach-xay-dung-cau-truc-du-lieu-stack-va-queue.html
+// định nghĩa kích thước của hàng đợi
+int size = 10;
+// đánh dấu vị trí đầu hàng đợi
+int front = 0;
+// đánh dấu vị trí cuối hàng đợi
+int rear = -1;
+// khai báo 1 mảng để tạo hàng đợi
+int queue[10];
+bool isEmpty() {
+    return (front < 0 || front > rear) ? true : false;
+}
+bool isFull() {
+    return rear == size - 1 ? true : false;
+}
+void enqueue(int item) {
+    if (isFull()) {
+        cout << "Hàng đợi đầy rùi má ơi, không enqueue được nữa đâu!" << endl;
+        return;
+    }
+    rear++;
+    queue[rear] = item;
+}
+void dequeue() {
+    if (isEmpty()) {
+        cout << "Uầy, hàng đợi rỗng má ơi, có gì đâu mà dequeue!" << endl;
+        return;
+    }
+    queue[front] = 0;
+    front++;
+}
+int peek() {
+    return queue[front];
+}
