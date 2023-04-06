ea98069cfabf49f1ffaed4b341536ce27b7b293d
letrongphuc07 <125432003+letrongphuc07@users.noreply.github.com>
2023-04-06 07:21:06 +0700

    Create CTDL&GT_Interchange_Sort_1.cpp

---
+++ CTDL&GT_Interchange_Sort_1.cpp
@@ -0,0 +1,12 @@
+nguồn tham khảo: https://chidokun.github.io/2015/03/interchange-sort/
+void InterchangeSort(int *a, int n)
+{
+   for (int i = 0; i < n-1; i++)
+      for (int j = i+1; j < n; j++)
+         if (a[i] > a[j])
+         {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+         }
+}
